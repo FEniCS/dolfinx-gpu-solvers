047dcbfc38f6c1ee361bdc0dffef666518f954b9
Chris Richardson <cnr12@cam.ac.uk>
2025-10-21 11:44:42 +0000

    Solve multiple times

--- demo_cudss/main.cpp
+++ demo_cudss/main.cpp
@@ -1,83 +1,3 @@
-// # Poisson equation
-//
-// This demo illustrates how to:
-//
-// * Solve a linear partial differential equation
-// * Create and apply Dirichlet boundary conditions
-// * Define Expressions
-// * Define a FunctionSpace
-//
-// ## Equation and problem definition
-//
-// The Poisson equation is the canonical elliptic partial differential
-// equation.  For a domain $\Omega \subset \mathbb{R}^n$ with boundary
-// $\partial \Omega = \Gamma_{D} \cup \Gamma_{N}$, the Poisson equation
-// with particular boundary conditions reads:
-//
-// \begin{align*}
-//    - \nabla^{2} u &= f \quad {\rm in} \ \Omega, \\
-//      u &= 0 \quad {\rm on} \ \Gamma_{D}, \\
-//      \nabla u \cdot n &= g \quad {\rm on} \ \Gamma_{N}. \\
-// \end{align*}
-//
-// Here, $f$ and $g$ are input data and $n$ denotes the outward directed
-// boundary normal. The most standard variational form of Poisson
-// equation reads: find $u \in V$ such that
-//
-// $$
-//    a(u, v) = L(v) \quad \forall \ v \in V,
-// $$
-// where $V$ is a suitable function space and
-//
-// \begin{align*}
-//    a(u, v) &= \int_{\Omega} \nabla u \cdot \nabla v \, {\rm d} x, \\
-//    L(v)    &= \int_{\Omega} f v \, {\rm d} x
-//    + \int_{\Gamma_{N}} g v \, {\rm d} s.
-// \end{align*}
-//
-// The expression $a(u, v)$ is the bilinear form and $L(v)$ is the
-// linear form. It is assumed that all functions in $V$ satisfy the
-// Dirichlet boundary conditions ($u = 0 \ {\rm on} \ \Gamma_{D}$).
-//
-// In this demo, we shall consider the following definitions of the
-// input functions, the domain, and the boundaries:
-//
-// * $\Omega = [0,1] \times [0,1]$ (a unit square)
-// * $\Gamma_{D} = \{(0, y) \cup (1, y) \subset \partial \Omega\}$
-// (Dirichlet boundary)
-// * $\Gamma_{N} = \{(x, 0) \cup (x, 1) \subset \partial \Omega\}$
-// (Neumann boundary)
-// * $g = \sin(5x)$ (normal derivative)
-// * $f = 10\exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)$ (source term)
-//
-//
-// ## Implementation
-//
-// The implementation is split in two files: a file containing the
-// definition of the variational forms expressed in UFL and a C++ file
-// containing the actual solver.
-//
-// Running this demo requires the files: {download}`demo_poisson/main.cpp`,
-// {download}`demo_poisson/poisson.py` and
-// {download}`demo_poisson/CMakeLists.txt`.
-//
-// ### UFL code
-//
-// The UFL code is implemented in {download}`demo_poisson/poisson.py`.
-// ````{admonition} UFL code implemented in Python
-// :class: dropdown
-// ![ufl-code]
-// ````
-//
-// ### C++ program
-//
-// The main solver is implemented in the
-// {download}`demo_poisson/main.cpp` file.
-//
-// At the top we include the DOLFINx header file and the generated
-// header file "Poisson.h" containing the variational forms for the
-// Poisson equation.  For convenience we also include the DOLFINx
-// namespace.
 
 #include "poisson.h"
 #include <basix/finite-element.h>
@@ -95,17 +15,6 @@
 using namespace dolfinx;
 using T = PetscScalar;
 using U = typename dolfinx::scalar_value_t<T>;
-
-// Then follows the definition of the coefficient functions (for $f$ and
-// $g$), which are derived from the {cpp:class}`Expression` class in
-// DOLFINx
-
-// Inside the `main` function, we begin by defining a mesh of the
-// domain. As the unit square is a very standard domain, we can use a
-// built-in mesh provided by the {cpp:class}`UnitSquareMesh` factory. In
-// order to create a mesh consisting of 32 x 32 squares with each square
-// divided into two triangles, and the finite element space (specified
-// in the form file) defined relative to this mesh, we do as follows:
 
 int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);
@@ -127,12 +36,6 @@ int main(int argc, char *argv[]) {
         std::make_shared<fem::FunctionSpace<U>>(fem::create_functionspace<U>(
             mesh, std::make_shared<fem::FiniteElement<U>>(element)));
 
-    //  Next, we define the variational formulation by initializing the
-    //  bilinear and linear forms ($a$, $L$) using the previously
-    //  defined {cpp:class}`FunctionSpace` `V`.  Then we can create the
-    //  source and boundary flux term ($f$, $g$) and attach these to the
-    //  linear form.
-
     // Prepare and set Constants for the bilinear form
     auto kappa = std::make_shared<fem::Constant<T>>(2.0);
     auto f = std::make_shared<fem::Function<T>>(V);
@@ -143,17 +46,6 @@ int main(int argc, char *argv[]) {
                                          {{"kappa", kappa}}, {}, {});
     fem::Form<T> L = fem::create_form<T>(*form_poisson_L, {V},
                                          {{"f", f}, {"g", g}}, {}, {}, {});
-
-    //  Now, the Dirichlet boundary condition ($u = 0$) can be created
-    //  using the class {cpp:class}`DirichletBC`. A
-    //  {cpp:class}`DirichletBC` takes two arguments: the value of the
-    //  boundary condition, and the part of the boundary on which the
-    //  condition applies. In our example, the value of the boundary
-    //  condition (0) can represented using a {cpp:class}`Function`,
-    //  and the Dirichlet boundary is defined by the indices of degrees
-    //  of freedom to which the boundary condition applies. The
-    //  definition of the Dirichlet boundary condition then looks as
-    //  follows:
 
     // Define boundary condition
 
@@ -192,13 +84,6 @@ int main(int argc, char *argv[]) {
           return {f, {f.size()}};
         });
 
-    //  Now, we have specified the variational forms and can consider
-    //  the solution of the variational problem. First, we need to
-    //  define a {cpp:class}`Function` `u` to store the solution. (Upon
-    //  initialization, it is simply set to the zero function.) Next, we
-    //  can call the `solve` function with the arguments `a == L`, `u`
-    //  and `bc` as follows:
-
     auto u = std::make_shared<fem::Function<T>>(V);
     la::SparsityPattern sp = fem::create_sparsity_pattern(a);
     sp.finalize();
@@ -210,6 +95,7 @@ int main(int argc, char *argv[]) {
     A.scatter_rev();
     fem::set_diagonal<T>(A.mat_set_values(), *V, {bc});
 
+    // Copy matrix to GPU device
     la::MatrixCSR<T, thrust::device_vector<T>,
                   thrust::device_vector<std::int32_t>,
                   thrust::device_vector<std::int32_t>>
@@ -221,6 +107,7 @@ int main(int argc, char *argv[]) {
     b.scatter_rev(std::plus<T>());
     bc.set(b.array(), std::nullopt);
 
+    // Copy vectors to GPU device
     la::Vector<T, thrust::device_vector<T>> b_device(b);
     la::Vector<T, thrust::device_vector<T>> u_device(*(u->x()));
 
@@ -275,10 +162,11 @@ int main(int argc, char *argv[]) {
 
     //---------------------------------------------------------------------------------
     // Solving the system
-    dolfinx::common::Timer tsolve3("Solve CUDSS - solve");
-    cudssExecute(handle, CUDSS_PHASE_SOLVE, config, data, A_dss, u_dss, b_dss);
-    tsolve3.stop();
-    tsolve3.flush();
+    for (int i = 0; i < 100; ++i) {
+      dolfinx::common::Timer tsolve3("Solve CUDSS - solve");
+      cudssExecute(handle, CUDSS_PHASE_SOLVE, config, data, A_dss, u_dss,
+                   b_dss);
+    }
 
     // Update ghost values before output
     // u->x()->scatter_fwd();
@@ -288,7 +176,7 @@ int main(int argc, char *argv[]) {
     //  the solution to a `VTK` file (specified using the suffix `.pvd`)
     //  for visualisation in an external program such as Paraview.
 
-    dolfinx::common::Timer tsolve4("Solve CUDSS - copy back");
+    dolfinx::common::Timer tsolve4("Solve CUDSS - copy back to CPU");
     // Copy back to host
     thrust::copy(u_device.array().begin(), u_device.array().end(),
                  u->x()->array().begin());
