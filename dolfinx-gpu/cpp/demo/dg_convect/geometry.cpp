028ed01313c3ccf48fbbbef44e809d4c31b0ee21
Chris Richardson <cnr12@cam.ac.uk>
2026-05-12 14:33:10 +0000

    Remove some debug

--- dolfinx-gpu/cpp/demo/dg_convect/geometry.cpp
+++ dolfinx-gpu/cpp/demo/dg_convect/geometry.cpp
@@ -35,6 +35,7 @@ compute_facet_normals(dolfinx::mesh::Mesh<double>& mesh,
     }
   }
 
+  // Compute facet Jacobians/normals
   std::vector<T> facet_jacobians(num_facets * 3, 0.0);
   auto xgeom = mesh.geometry().x();
   auto dofmap = mesh.geometry().dofmap();
@@ -44,11 +45,7 @@ compute_facet_normals(dolfinx::mesh::Mesh<double>& mesh,
     T coord_dofs[4][3];
     for (int i = 0; i < 4; ++i)
       for (int j = 0; j < 3; ++j)
-      {
         coord_dofs[i][j] = xgeom[dofmap(c, i) * 3 + j];
-        std::cout << "coord[" << i << "," << j << "]=" << coord_dofs[i][j]
-                  << "\n";
-      }
 
     // One quadrature point per facet for now...
     for (int f = 0; f < num_facets_per_cell; ++f)
@@ -56,6 +53,9 @@ compute_facet_normals(dolfinx::mesh::Mesh<double>& mesh,
       std::int32_t idx = facet_list[c * num_facets_per_cell + f];
       if (idx >= 0)
       {
+        // For linear cells, J is same everywhere, but will vary
+        // across cell for higher-order.
+
         T J[3][3];
         for (int i = 0; i < 3; i++)
         {
@@ -65,14 +65,6 @@ compute_facet_normals(dolfinx::mesh::Mesh<double>& mesh,
             for (int k = 0; k < 4; k++)
               J[i][j] += coord_dofs[k][i] * dphi[j * 16 + f * 4 + k];
           }
-        }
-
-        for (int i = 0; i < 3; ++i)
-        {
-          std::cout << "J=[";
-          for (int j = 0; j < 3; ++j)
-            std::cout << J[i][j] << " ";
-          std::cout << "]\n";
         }
 
         using Vec3 = std::array<T, 3>;
@@ -89,25 +81,15 @@ compute_facet_normals(dolfinx::mesh::Mesh<double>& mesh,
         Vec3 g2 = {J[0][1], J[1][1], J[2][1]};
         Vec3 g3 = {J[0][2], J[1][2], J[2][2]};
 
-        // Outward scaled normals for each facet (facet i is opposite vertex i):
-        //   Facet 0 (v1,v2,v3): tangents (g2-g1),(g3-g1)  -> (g2-g1) x (g3-g1)
-        //   Facet 1 (v0,v2,v3): tangents g2, g3            -> g3 x g2
-        //   Facet 2 (v0,v1,v3): tangents g1, g3            -> g1 x g3
-        //   Facet 3 (v0,v1,v2): tangents g1, g2            -> g2 x g1
-        Vec3 dg21 = {g2[0] - g1[0], g2[1] - g1[1], g2[2] - g1[2]};
-        Vec3 dg31 = {g3[0] - g1[0], g3[1] - g1[1], g3[2] - g1[2]};
-
+        // Normal vectors from Jacobian - magnitude is detJ(facet)
         std::array<Vec3, 4> n;
-        n[0] = cross(dg21, dg31);
         n[1] = cross(g3, g2);
         n[2] = cross(g1, g3);
         n[3] = cross(g2, g1);
+        n[0] = {-n[1][0] - n[2][0] - n[3][0], -n[1][1] - n[2][1] - n[3][1],
+                -n[1][2] - n[2][2] - n[3][2]};
 
-        for (int i = 0; i < 4; ++i)
-          std::cout << "n[" << i << "] = " << n[i][0] << "," << n[i][1] << ","
-                    << n[i][2] << "\n";
-
-        facet_jacobians[idx * 3]     = n[f][0];
+        facet_jacobians[idx * 3] = n[f][0];
         facet_jacobians[idx * 3 + 1] = n[f][1];
         facet_jacobians[idx * 3 + 2] = n[f][2];
       }
