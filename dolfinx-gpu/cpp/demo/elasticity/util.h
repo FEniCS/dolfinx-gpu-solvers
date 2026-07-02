f4dea9cfbc7c0ce4677428a5e01ceea78451c877
Chris Richardson <cnr12@cam.ac.uk>
2026-07-02 09:52:59 +0000

    Fix util.h

--- dolfinx-gpu/cpp/demo/elasticity/util.h
+++ dolfinx-gpu/cpp/demo/elasticity/util.h
@@ -8,20 +8,13 @@
 #include <sstream>
 #include <string>
 
-#ifdef USE_HIP
+#if defined(__HIP__)
 #include <hip/hip_runtime.h>
-#elif USE_CUDA
+#else
 #include <cuda_runtime.h>
 #endif
 
-#if defined(USE_CUDA) || defined(USE_HIP)
-
-// Some useful utilities for error checking and synchronisation
-// for each hardware type
-
-namespace benchdolfinx
-{
-#ifdef USE_HIP
+#if defined(__HIP__)
 #define err_check(command)                                                     \
   {                                                                            \
     hipError_t status = command;                                               \
@@ -32,7 +25,7 @@ namespace benchdolfinx
       exit(1);                                                                 \
     }                                                                          \
   }
-#elif USE_CUDA
+#else
 #define err_check(command)                                                     \
   {                                                                            \
     cudaError_t status = command;                                              \
@@ -45,22 +38,16 @@ namespace benchdolfinx
   }
 #endif
 
-#ifdef USE_HIP
+#if defined(__HIP__)
 #define non_temp_load(addr) __builtin_nontemporal_load(addr)
 #define deviceMemcpyToSymbol(symbol, addr, count)                              \
   hipMemcpyToSymbol(symbol, addr, count)
 inline void check_device_last_error() { err_check(hipGetLastError()); }
 inline void device_synchronize() { err_check(hipDeviceSynchronize()); }
-#elif USE_CUDA
+#else
 #define non_temp_load(addr) __ldg(addr)
 #define deviceMemcpyToSymbol(symbol, addr, count)                              \
   cudaMemcpyToSymbol(symbol, addr, count)
 inline void check_device_last_error() { err_check(cudaGetLastError()); }
 inline void device_synchronize() { err_check(cudaDeviceSynchronize()); }
-#else
-#error "Unsupported platform"
-#endif
-
-std::string get_device_information();
-} // namespace benchdolfinx
 #endif
